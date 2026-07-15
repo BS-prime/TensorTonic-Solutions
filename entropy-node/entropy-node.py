0f4a9a73ce9d7e95f4a8e86e0ce580045d91438b
Bhargav Sharma <bhargav.sharma548@gmail.com>
2026-07-16 01:35:32 +0530

    Solve entropy-node

--- entropy-node/entropy-node.py
+++ entropy-node/entropy-node.py
@@ -18,5 +18,5 @@ def entropy_node(y):
     p_i = freq/np.sum(freq)
 
     # 3. calculate entropy
-    log_p = np.where(p_i > 0, np.log2(p_i), 0.0)
+    log_p = np.where(p_i > 0, np.log2(p_i), 0.0) # simply because of the docstring by tensortonic; otherwise, np.unique doesn't let zero-count classes pass through.
     return - np.sum(p_i * log_p) 
