4c9b7a268c2f363ec7f86b8723722dffcd226f4c
Bhargav Sharma <bhargav.sharma548@gmail.com>
2026-07-16 23:42:16 +0530

    Solve leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -7,8 +7,9 @@ def leaky_relu(x, alpha=0.01):
     # 0. convert to array
     x = np.asarray(x)
 
+    # 1. perform checks
     if x.size == 0:
         raise ValueError("x is empty")
 
-    # 1. leaky relu
+    # 2. leaky relu
     return np.where(x >= 0, x, alpha*x)
