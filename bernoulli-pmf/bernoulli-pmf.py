03c7e9ee7f557a583cbb64287ddb2161cf7514fe
Bhargav Sharma <bhargav.sharma548@gmail.com>
2026-08-04 00:24:44 +0530

    Solve bernoulli-pmf

--- bernoulli-pmf/bernoulli-pmf.py
+++ bernoulli-pmf/bernoulli-pmf.py
@@ -7,6 +7,12 @@ def bernoulli_pmf_and_moments(x, p):
     # 1. convert to array
     x = np.asarray(x, dtype=np.int64)
 
+    if not np.all((x == 0) | (x == 1)):
+        raise ValueError("x must contain only 0 and 1.")
+
+    if not (0 <= p <= 1):
+        raise ValueError("p must be between 0 and 1.")
+        
     # 2. calculate pmf
     pmf = np.where(x==1, p, 1-p)
 
