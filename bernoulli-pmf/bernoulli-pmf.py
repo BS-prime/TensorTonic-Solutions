a7dca53c96670ed6c40831a7f36775b64bb8a67f
Bhargav Sharma <bhargav.sharma548@gmail.com>
2026-08-11 00:28:15 +0530

    Solve bernoulli-pmf

--- bernoulli-pmf/bernoulli-pmf.py
+++ bernoulli-pmf/bernoulli-pmf.py
@@ -4,23 +4,27 @@ def bernoulli_pmf_and_moments(x, p):
     """
     Compute Bernoulli PMF and distribution moments.
     """
-    # 1. convert to array
+    # 0. convert to numpy array
     x = np.asarray(x, dtype=np.int64)
 
-    if not np.all((x == 0) | (x == 1)):
-        raise ValueError("x must contain only 0 and 1.")
+    # 1. input validation
+    if not 0 <= p <= 1:
+        raise ValueError("probability should be between 0 and 1")
+    if x.size == 0:
+        raise ValueError("inputs can't be empty")
 
-    if not (0 <= p <= 1):
-        raise ValueError("p must be between 0 and 1.")
-        
-    # 2. calculate pmf
-    pmf = np.where(x==1, p, 1-p)
+    if x.ndim > 1:
+        raise ValueError("Both parameter should of same dimension")
 
-    # 3. mean
+    # 2. calculate PMF
+    pmf = np.where(
+        x == 1,
+        p,
+        1-p
+    )
+
+    # 3. calculate mean and varience
     mean = p
-
-    # 4. variance 
-    var = mean * (1 - mean)
+    var = p * (1-p)
 
     return pmf, mean, var
-    
