0a6c1817366dbbe2786e4535325d38d4719529ab
Bhargav Sharma <bhargav.sharma548@gmail.com>
2026-08-12 01:25:11 +0530

    Solve geometric-pmf-mean

--- geometric-pmf-mean/geometric-pmf-mean.py
+++ geometric-pmf-mean/geometric-pmf-mean.py
@@ -8,7 +8,7 @@ def geometric_pmf_mean(k, p):
     k = np.asarray(k)
     
     # 0. validate param
-    if (k < 0).any():
+    if (k < 1).any():
         raise ValueError("Trails could not less zero")
 
     if not 0 < p <= 1:
