05a5ba61c6a0629a3764968afb5b9e521b1500ac
Bhargav Sharma <bhargav.sharma548@gmail.com>
2026-08-12 00:32:03 +0530

    Solve poisson-pmf-cdf

--- poisson-pmf-cdf/poisson-pmf-cdf.py
+++ poisson-pmf-cdf/poisson-pmf-cdf.py
@@ -5,11 +5,22 @@ def poisson_pmf_cdf(lam, k):
     """
     Compute Poisson PMF and CDF.
     """
+
+    # 0. validate params
+    if lam < 0:
+        raise ValueError("lamda must be non-negative")
+
+    if k < 0 or int(k) != k:
+        raise ValueError("k must be non-negative interger")
+        
+    k = int(k)
+    
     # 1. calculate pmf
     pmf = (lam**k * np.exp(-lam)) / factorial(k)
 
     # 2. calculate pmf
-    cdf = 0
+    cdf = 0.0
+    
     for i in range(k+1):
         cdf += (lam**i * np.exp(-lam)) / factorial(i)
 
