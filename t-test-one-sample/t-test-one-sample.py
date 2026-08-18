0bbe75142db0689c0fc47db9cb35ba18e9f34756
Bhargav Sharma <bhargav.sharma548@gmail.com>
2026-08-18 13:57:48 +0530

    Solve t-test-one-sample

--- t-test-one-sample/t-test-one-sample.py
+++ t-test-one-sample/t-test-one-sample.py
@@ -7,19 +7,19 @@ def t_test_one_sample(x, mu0):
     # 0. convert to numpy array
     x = np.asarray(x, dtype=np.float64)
 
-    # 1. validate the params
-    if x.size < 1 or mu0 is None:
-        raise ValueError("parameters can't be empty")
+    # 1. validate parameters
+    if x.size < 2 or mu0 is None:
+        raise ValueError("x must contain at least 2 observations and mu0 cannot be None")
 
     if x.ndim != 1:
         raise ValueError("x could not be more than 1 dimensional")
 
-    # 2. calculate mean, and total length of the sample
+    # 2. calculate sample mean and sample size
     s_mean = np.mean(x)
     s_n = len(x)
 
-    # 3. calculate sample mean Bessel correction
-    s_std = np.sqrt(1 / (s_n - 1) * (np.sum((x - s_mean) ** 2)))
+    # 3. calculate sample standard deviation using Bessel's correction
+    s_std = np.sqrt(np.sum((x - s_mean) ** 2) / (s_n - 1))
 
-    # 4. calculate t_test_one_sample
+    # 4. calculate one-sample t-statistic
     return (s_mean - mu0) / (s_std / np.sqrt(s_n))
